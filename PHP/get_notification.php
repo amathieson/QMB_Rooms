8f70008e67b23db804aea8a38ee54f4cec88339a
Adam Mathieson <adam@mathieson.dev>
2023-11-19 03:29:56 +0000

    + Notifications are dynamic  + A little better safety for if the data fails to pull

---
+++ PHP/get_notification.php
@@ -0,0 +1,3 @@
+<?php
+include_once "api_header.php";
+echo json_encode(["notification"=>"An SSL issue has been reported with the university's timetable server - the QMB_Rooms platform is temporarily unavailable."]);
--- QMB_Rooms/src/App.vue
+++ QMB_Rooms/src/App.vue
@@ -6,11 +6,11 @@ import Timetable_viewer from "./components/timetable_viewer.vue";
   <header>QMB_Rooms</header>
   <div class="view">
     <Transition name="notice">
-    <div class="room-note" v-if="notes[rooms[room].name] !== undefined">Please Note - {{notes[rooms[room].name]}}</div>
+    <div class="room-note" v-if="rooms[room] !== undefined && notes[rooms[room].name] !== undefined">Please Note - {{notes[rooms[room].name]}}</div>
     </Transition>
-    <marquee class="note"><i>Platform in early access please excuse any issues.</i></marquee>
+    <marquee class="note"><i>{{notification}}</i></marquee>
 
-      <div class="room-card">
+      <div class="room-card" v-if="rooms[room] !== undefined">
         <Transition mode="out-in">
           <h3 :key="rooms[room].name">{{ rooms[room].name.startsWith("Lab") ? "Lab" : "" }}</h3>
         </Transition>
@@ -39,7 +39,7 @@ import Timetable_viewer from "./components/timetable_viewer.vue";
       <sub class="foot">Last Updated: <code>{{dateFormat(last_fetch)}}</code> - Data Pulled: <code>{{dateFormat(data_pulled*1000)}}</code></sub>
   </div>
   <div class="fab icon" @click="showtimetable=true">📅</div>
-  <timetable_viewer ref="timetable" :open="showtimetable" @close="showtimetable = false" :room="rooms[room].next.roomID"></timetable_viewer>
+  <timetable_viewer ref="timetable" :open="showtimetable" @close="showtimetable = false" :room="rooms[room]?.next?.roomID"></timetable_viewer>
 </template>
 
 <style scoped>
@@ -179,6 +179,7 @@ export default {
   },
   data: ()=>{
     return {
+      "notification": "Platform in early access please excuse any issues.",
       "showtimetable":false,
       "data_pulled":0,
       "last_fetch":0,
@@ -224,6 +225,7 @@ export default {
   },
   methods: {
     fetchData: (that)=>{
+      fetch(that.root + "/get_notification.php").then((d)=>{d.json().then((dta)=>{that.notification = dta.notification})});
       fetch(that.root + "/get_notices.php").then((d)=>{d.json().then((dta)=>{that.notes = dta})});
       fetch( that.root + "/get_availabilities.php").then((d)=>{d.json().then((dta)=>{
         let arr = [];
