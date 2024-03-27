1000c3e07b8676f7ba455d1db2bf7ec779639b0a
Adam Mathieson <adam@mathieson.dev>
2024-03-27 16:25:41 +0000

    + Start of Digital Signage Project

---
+++ digital-signage/src/App.vue
@@ -0,0 +1,20 @@
+<script setup>
+</script>
+
+<template>
+  <div class="container">
+    <div class="image-container">
+      <img src="">
+    </div>
+    <img src="" class="logo">
+    <h1>Lab 0</h1>
+    <div class="lower-section">
+      <h2>Big Data Analysis module (AC51011)</h2>
+      <sub>Samuel, Oluwafemi,Janjic, Vladimir,z A N Other (Comp 1)</sub>
+      <h3>09:00</h3>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+</style>
